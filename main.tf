0cab8dbfc13ec471d4edfde002abd9d4ca8bb204
Isaiah Fielder <isaiahmfielder@gmail.com>
2020-12-07 10:19:05 -0700

    add inital logic

---
+++ main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 2.70"
+    }
+  }
+}
+
+
+provider "aws" {
+  profile = "sliceabread"
+  region  = "us-east-1"
+}
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_lambda_function" "example" {
+  function_name = "example-tf-from-actions"
+  handler       = "index.handler"
+  runtime       = "nodejs12.x"
+  filename      = "dist.zip"
+}
