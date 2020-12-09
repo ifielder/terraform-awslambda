ff7a76cb85f1cc697acbe6d9f763b46f6f1c808c
Isaiah Fielder <isaiahmfielder@gmail.com>
2020-12-09 11:46:43 -0700

    add source_code_hash for updates

--- terraform/main.tf
+++ terraform/main.tf
@@ -39,9 +39,10 @@ resource "aws_iam_role_policy_attachment" "terraform_lambda_policy" {
 
 
 resource "aws_lambda_function" "example" {
-  function_name = "example-tf-from-actions"
-  handler       = "dist/index.handler"
-  runtime       = "nodejs12.x"
-  filename      = "dist.zip"
-  role          = aws_iam_role.iam_for_lambda.arn
+  function_name    = "example-tf-from-actions"
+  handler          = "dist/index.handler"
+  runtime          = "nodejs12.x"
+  filename         = "../dist.zip"
+  source_code_hash = filebase64sha256("../dist.zip")
+  role             = aws_iam_role.iam_for_lambda.arn
 }
