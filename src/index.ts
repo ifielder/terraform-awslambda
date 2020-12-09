84af727caaf6adbf567733cde7c1a669a24aaf3f
Isaiah Fielder <isaiahmfielder@gmail.com>
2020-12-09 11:37:38 -0700

    update build

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
-export const handler = async (event: any) => {
-  console.log(JSON.stringify(event));
+import cuid from "cuid";
 
+export const handler = async (event: any) => {
+  console.log(cuid());
+  console.log(JSON.stringify(event));
   return "success";
 };
